8c7d17c3e3fe362e932f9c090be805996d6342a4
Marloes van der Zalm <marloes@MacBook-Pro-van-Marloes.local>
2022-11-02 21:35:55 +0100

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Welcome to van der Zalm Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Cookies"
+        src="../images/cookies.jpeg"
+      />
       </Layout>
   )
 }
-// Stap 3: Exporteer je component
+
 export default IndexPage
