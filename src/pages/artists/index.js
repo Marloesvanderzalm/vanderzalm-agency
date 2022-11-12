d1c261a3504987123f84fe429296c88d9a9b2e4c
Marloes van der Zalm <marloes@MacBook-Pro-van-Marloes.local>
2022-11-12 21:53:10 +0100

    tot en met toevoegen dynamische afbeeldingen

---
+++ src/pages/artists/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
+const ArtistsPage = ({data: {allWpArtist: {edges}}}) => {
+  return (
+    <Layout pageTitle="Artists of van der Zalm Agency">
+      {edges.map((item) => {
+        const artist = item.node.artistMeta;
+        const slug = item.node.slug;
+        return <Link to={`/artists/${slug}`}>
+            <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+          </Link>
+        
+      })}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+   allWpArtist {
+    edges {
+      node {
+        id
+        slug
+        artistMeta {
+          firstName
+          lastName
+          artistName
+        }
+      }
+    }
+  }
+}
+
+`
+
+export default ArtistsPage
