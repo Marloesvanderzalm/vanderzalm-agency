2363acff9d7d63c1f99bba34e73c78642ce5d372
Marloes van der Zalm <marloes@MacBook-Pro-van-Marloes.local>
2022-11-23 17:52:25 +0100

    cursus t /m hoofdstuk homepage opmaak en styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,51 +8,64 @@ import {
   navLinkText, 
   siteTitle 
 } from './layout.module.css'
+import Footer from './footer'
 
-const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
+  // Imports
+
+  const Layout = ({ children }) => {
+
+    const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
           title
         }
       }
+      wpPage(slug: { eq: "contact-us" }) {
+        contactUsFields {
+            address
+            city
+            zipCode
+            facebook
+            instagram
+        }
+      }
     }
   `)
-
-  return (
-    <div className={container}>
-      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <nav className={nav}>
-        <header className={siteTitle}>
-          <h1>{data.site.siteMetadata.title}</h1>
-        </header>
-        <ul className={navLinks}>
-        <li>
-        </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/artists">
-              Artists
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
-  )
-}
-
-export default Layout
+  
+    return (
+      <>
+        <div className={container}>
+          <title>{data.site.siteMetadata.title}</title>
+          <nav className={nav}>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+            <ul className={navLinks}>
+              <li></li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/about">
+                  About
+                </Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link className={navLinkText} to="/artists">
+                  Artists
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <main>{children}</main>
+        </div>
+        <Footer
+          siteTitle={data.site.siteMetadata.title}
+          companyInfo={data.wpPage.contactUsFields}
+        />
+      </>
+    )
+  }
+  
+  export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,100 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  CTA,
+  section,
+  subtitle,
+  artists
+} from "../page.module.css"
+import Artist from '../components/artists'
 
-const IndexPage = () => {
+const IndexPage = ({ data: {wpPage: { homeFields }}}) => {
+  const image = getImage(homeFields.picture.localFile)
   return (
-      <Layout pageTitle="Welcome to van der Zalm Agency!">
-      <p>Lorem ipsum</p>
-      <StaticImage
-        alt="Cookies"
-        src="../images/cookies.jpeg"
-      />
+      <Layout>
+        <section className={header}>
+          <article className={headerInfo}>
+            <h1 className={headerTitle}>{homeFields.title}</h1>
+            <div 
+              dangerouslySetInnerHTML={{
+                __html: homeFields.description,
+              }} 
+            />
+            <a className={CTA} target="__blank" href={homeFields.callToAction.url}>
+              {homeFields.callToAction.title}
+            </a>
+          </article>
+          <div>
+            <GatsbyImage
+              image={image}
+              className={headerPicture}
+              alt={homeFields.picture.altText}
+            />
+          </div>
+        </section>
+        <section className={section}>
+          <h2 className={subtitle}>Featured Artists</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed doo eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <article className={artists}>
+            {homeFields.artists.map(artist => {
+              return <Artist key={artist.id} artist={artist} slug={`artists/${artist.slug}`} />
+            })}
+          </article>
+        </section>
       </Layout>
   )
 }
+
+export const query = graphql `
+query {
+  wpPage(slug: {eq: "home"}) {
+    homeFields {
+      artists {
+        ... on WpArtist {
+          id
+          slug
+          artistMeta {
+            artistName
+            firstName
+            lastName
+            profilePicture {
+              altText
+              localFile {
+                childImageSharp {
+                  gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+                }
+              }
+            }
+          }
+        }
+      }
+      callToAction {
+        title
+        url
+      }
+      description
+      title
+      picture {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+    }
+  }
+}
+
+`
 
 export default IndexPage
