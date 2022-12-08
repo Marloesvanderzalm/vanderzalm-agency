d2b61aa72bc70c6283bdf2352965ee7854edff08
Marloes van der Zalm <marloesvdzalm@gmail.com>
2022-12-08 19:46:55 +0100

    volledig afgewerkt voorbeeldproject Artist Agency

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,10 @@ import {
     socials,
     facebook,
     instagram,
-    link
+    link,
+    form,
+    formLabel,
+
   } from "../page.module.css"
 
   const ContactPage = ({
@@ -64,8 +67,20 @@ import {
             alt={contactUsFields.picture.altText}
           />
         </section>
-      </Layout>
-    )
+        <section className={form}>
+        <form name="contact" method="POST" data-netlify="true">
+            <label>Your Name:</label>
+            <input type="text" name="name" required={true} />
+            <label>Your Email:</label>
+            <input type="email" name="email" required={true} />
+            <label>Message:</label>
+            <textarea name="message" required={true}></textarea>
+            <input type="hidden" name="form-name" value="contact" />
+            <button type="submit">Send</button>
+        </form>
+      </section>
+    </Layout>
+  )
 }
 
 export default ContactPage;
